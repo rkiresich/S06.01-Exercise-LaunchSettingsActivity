1f0d4d8f91a5df8042e024ba6a3eb01deda869d2
Ricky K <rkiresich@gmail.com>
2018-03-21 22:04:13 -0600

    now works, accidently created 2 activity settings.

--- app/src/main/java/com/example/android/sunshine/SettingsActivity.java
+++ app/src/main/java/com/example/android/sunshine/SettingsActivity.java
@@ -9,7 +9,7 @@ public class SettingsActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_settings2);
+        setContentView(R.layout.activity_settings);
         this.getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
